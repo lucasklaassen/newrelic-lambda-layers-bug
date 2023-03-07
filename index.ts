2ebf3611be34e382067107d73bcd30cc4802e597
lucasklaassen <lucasklaassen1@gmail.com>
2023-03-06 21:21:31 -0800

    chore(): init commit

---
+++ index.ts
@@ -0,0 +1,48 @@
+import { Client } from "pg";
+import newrelic from 'newrelic';
+
+const main = async (event: any) => {
+  let results;
+  const text = `
+    SELECT *
+    FROM public.table
+  `
+  const values: any[] = [];
+  try {
+    const configurations: any = {
+      user: process.env.POSTGRES_USERNAME,
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      database: process.env.POSTGRES_DATABASE,
+      password: process.env.POSTGRES_PASSWORD,
+      connectionTimeoutMillis: 5000,
+      idleTimeoutMillis: 30000,
+      max: 10,
+      ssl: {
+        rejectUnauthorized: true,
+      },
+      allowExitOnIdle: true,
+    };
+    const client = new Client(configurations);
+    await client.connect();
+    results = await client.query(text, values);
+  } catch(error) {
+    console.log('ERROR', error);
+  }
+  
+  console.log('RESULTS', results);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+}
+
+module.exports.handler = newrelic.setLambdaHandler(main);
