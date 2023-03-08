a8079f89fd4e35b430499cbc0e3f2d29c13d22dc
lucasklaassen <lucasklaassen1@gmail.com>
2023-03-07 19:29:04 -0800

    feat(): add middy and postgres

--- index.ts
+++ index.ts
@@ -1,6 +1,55 @@
-import newrelic from 'newrelic';
+import { Client } from "pg";
+const middy = require('middy');
 
-const main = async (event: any) => {
+const logging = () => ({
+  before: (handler: any, next: Function) => {
+    console.log('before', handler);
+
+    return next();
+  },
+  after: (handler: any, next: Function) => {
+    console.log('after', handler);
+
+    return next();
+  },
+  onError: (handler: any, next: Function) => {
+    console.log('onError', handler);
+
+    return next();
+  },
+});
+
+const main = async (event: any, context: any) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  let results;
+  const text = `
+    SELECT 1
+  `
+  const values: any[] = [];
+  try {
+    const configurations: any = {
+      user: process.env.POSTGRES_USERNAME,
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      database: process.env.POSTGRES_DATABASE,
+      password: process.env.POSTGRES_PASSWORD,
+      connectionTimeoutMillis: 5000,
+      idleTimeoutMillis: 30000,
+      max: 10,
+      ssl: {
+        rejectUnauthorized: true,
+      },
+      allowExitOnIdle: true,
+    };
+    const client = new Client(configurations);
+    await client.connect();
+    results = await client.query(text, values);
+  } catch(error) {
+    console.log('ERROR', error);
+  }
+
+  console.log('RESULTS', results);
+
   return {
     statusCode: 200,
     body: JSON.stringify(
@@ -12,6 +61,9 @@ const main = async (event: any) => {
       2
     ),
   };
-}
+};
 
-module.exports.handler = newrelic.setLambdaHandler(main);
+const mainHandler = middy(main)
+  .use(logging())
+
+module.exports.handler = mainHandler;
